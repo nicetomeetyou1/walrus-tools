0991dd59054e21a48922a89217160ef74a78c6ed
bartosian <official@bartestnet.com>
2024-12-12 20:21:26 -0500

    Added Publisher and Aggregator dashboards

--- prometheus/entrypoint.sh
+++ prometheus/entrypoint.sh
@@ -2,9 +2,11 @@
 
 # Debug: Print environment variables
 echo "PROMETHEUS_TARGET=${PROMETHEUS_TARGET}"
-echo "WALRUS_TARGET=${WALRUS_TARGET}"
+echo "WALRUS_NODE_TARGET=${WALRUS_NODE_TARGET}"
+echo "WALRUS_AGGREGATOR_TARGET=${WALRUS_AGGREGATOR_TARGET}"
+echo "WALRUS_PUBLISHER_TARGET=${WALRUS_PUBLISHER_TARGET}"
 
 # Perform substitution with sed
-sed "s|\${PROMETHEUS_TARGET}|${PROMETHEUS_TARGET}|g; s|\${WALRUS_TARGET}|${WALRUS_TARGET}|g" /etc/prometheus/prometheus.yml.tmpl > /etc/prometheus/prometheus.yml
+sed "s|\${PROMETHEUS_TARGET}|${PROMETHEUS_TARGET}|g; s|\${WALRUS_NODE_TARGET}|${WALRUS_NODE_TARGET}|g; s|\${WALRUS_AGGREGATOR_TARGET}|${WALRUS_AGGREGATOR_TARGET}|g; s|\${WALRUS_PUBLISHER_TARGET}|${WALRUS_PUBLISHER_TARGET}|g" /etc/prometheus/prometheus.yml.tmpl > /etc/prometheus/prometheus.yml
 
 exec prometheus --config.file=/etc/prometheus/prometheus.yml
